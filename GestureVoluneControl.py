2cc9667e158bffe5c163969c854335373e6b529a
geekboi <85869106+geekboi777@users.noreply.github.com>
2021-07-30 20:41:28 +0530

    Update GestureVoluneControl.py

--- GestureVoluneControl.py
+++ GestureVoluneControl.py
@@ -3,23 +3,37 @@ import time
 import numpy as np
 import HandTrackingmodule as htm
 import math
-import osascript
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
 wCam,hCam = 640,480
 
-result = osascript.osascript('get volume settings')
-print(result)
-print(type(result))
-volInfo = result[1].split(',')
-outputVol = volInfo[0].replace('output volume:', '')
-print(outputVol)
 
 
 cap = cv2.VideoCapture(0)
 cap.set(3,wCam)
 cap.set(4,hCam)
 pTime=0
-
+vol=0
+volPer = 0
+volBar=400
 detector = htm.handDetector(detectionCon=0.7)
+
+
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+#volume.GetMute()
+#volume.GetMasterVolumeLevel()
+volRange=volume.GetVolumeRange()
+
+minVol = volRange[0]
+maxVol = volRange[1]
+
+
 
 while True:
     success, img = cap.read()
@@ -38,15 +52,23 @@ while True:
 
         length = math.hypot(x2-x1,y2-y1)
         print(length)
-
+        vol = np.interp(length,[50,300],[minVol,maxVol])
+        volBar = np.interp(length, [50, 300], [400,150])
+        volPer = np.interp(length, [50, 300], [0, 100])
+        print(int(length),vol)
+        volume.SetMasterVolumeLevel(vol, None)
         if length<50:
             cv2.circle(img, (cx, cy), 15, (0,255,0), cv2.FILLED)
+    cv2.rectangle(img,(50,150),(85,400),(255, 0, 0),3)
+    cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
+    cv2.putText(img, f'FPS: {int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+
 
     cTime = time.time()
     fps = 1/(cTime - pTime)
     pTime = cTime
 
-    cv2.putText(img,f'FPS: {int(fps)}',(40,70),cv2.FONT_HERSHEY_PLAIN,3,(255,0,0),3)
+    cv2.putText(img,f'FPS: {int(fps)}',(40,70),cv2.FONT_HERSHEY_PLAIN,2,(255,0,0),2)
 
     cv2.imshow("img",img)
     cv2.waitKey(1)
